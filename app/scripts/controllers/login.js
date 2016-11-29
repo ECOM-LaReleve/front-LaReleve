b51064a7ef4d6b21b0e7bf1a7e9c66f33d3d9d71
Quentin Dunand <quentindunand@gmail.com>
2016-11-29 09:31:25 +0100

    Login view created

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name laReleveApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the laReleveApp
+ */
+angular.module('laReleveApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
