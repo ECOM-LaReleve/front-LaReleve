f9cfced63b014e09218e6a45425b33a7d2a2424e
Quentin Dunand <quentindunand@gmail.com>
2016-12-01 19:04:28 +0100

    Add autocomplete field for "chef de famille" on menageCreation view

--- app/scripts/controllers/menagecreation.js
+++ app/scripts/controllers/menagecreation.js
@@ -8,10 +8,39 @@
  * Controller of the laReleveApp
  */
 angular.module('laReleveApp')
-  .controller('MenagecreationCtrl', function () {
+  .controller('MenagecreationCtrl', ['$scope', '$q', '$timeout', function ($scope, $q, $timeout) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.individus = ['boy', 'girl', 'dog', 'town', 'car', 'cat'].map(function(state) {
+    	return {
+          value: state.toLowerCase(),
+          display: state
+        };
+    });
+
+
+    $scope.querySearch = function (query) {
+      var results = query ? $scope.individus.filter( createFilterFor(query) ) : $scope.individus;
+      var deferred = $q.defer();
+      $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+      return deferred.promise;
+    };
+
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+
+
+  }]);
